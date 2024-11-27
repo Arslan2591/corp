af98774c2ae65f730adeca4cc711aeff924dbe7e
Arslan Syed <arslan2591@gmail.com>
2024-11-27 14:51:31 +0500

    initial commit

---
+++ src/app/performance/page.tsx
@@ -0,0 +1,8 @@
+export default function PerformancePage() {
+  return (
+    <div>
+      <h1>Performance</h1>
+      <p>Performance content</p>
+    </div>
+  );
+}
---
+++ src/app/scale/page.tsx
@@ -0,0 +1,8 @@
+export default function ScalePage() {
+  return (
+    <div>
+      <h1>Scale</h1>
+      <p>Scale content</p>
+    </div>
+  );
+}
